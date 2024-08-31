897ef5118c420b617a43660af339844cb34671bf
geovko <octobermes@gmail.com>
2024-08-30 23:10:17 -0700

    made most of the backend

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,72 @@
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+// GET all comments
+router.get("/", async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [{ model: Post }, { model: User }],
+    });
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET a single comment
+router.get("/:id", async (req, res) => {
+  try {
+    const commentData = await Comment.findByPk(req.params.id, {
+      include: [{ model: Post }, { model: User }],
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: "No comment found with that id!" });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Create a comment
+router.post("/", withAuth, async (req, res) => {
+  const now = Date(Date.now()).split(" ");
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+      post_id: req.params.id,
+      dateCreated: `${now[1]} ${now[2]}, ${now[3]}`,
+    });
+
+    res.status(200).json(newComment);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// Delete post
+router.delete("/:id", withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!commentData) {
+      res.status(404).json({ message: "No comment found with this id!" });
+      return;
+    }
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
